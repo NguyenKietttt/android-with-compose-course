9248697a29ce021ee10dcd526de98ebff5651178
Kndt <nkiet84@gmail.com>
2025-10-14 07:27:24 +0700

    Update image for Happy Birthday project

--- happy-birthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ happy-birthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -4,9 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -14,6 +15,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -27,12 +31,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             HappyBirthdayTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    GreetingText(
-                        message = "Happy Birthday Sam!",
-                        from = "From Emma",
-                        modifier = Modifier
-                            .padding(innerPadding)
-                            .fillMaxSize())
+                    GreetingImage(
+                        message = stringResource(R.string.happy_birthday_text),
+                        from = stringResource(R.string.signature_text),
+                        modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -56,7 +58,27 @@ fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
             fontSize = 36.sp,
             modifier = Modifier
                 .padding(16.dp)
-                .align(Alignment.End)
+                .align(Alignment.CenterHorizontally)
+        )
+    }
+}
+
+@Composable
+fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.androidparty)
+    Box(modifier = modifier) {
+        Image(
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            alpha = 0.5f
+        )
+        GreetingText(
+            message = message,
+            from = from,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp)
         )
     }
 }
@@ -66,12 +88,11 @@ fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
 fun HappyBirthdayCardPreview() {
     HappyBirthdayTheme {
         Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-            GreetingText(
-                message = "Happy Birthday Sam!",
-                from = "From Emma",
-                modifier = Modifier
-                    .padding(innerPadding)
-                    .fillMaxSize())
+            GreetingImage(
+                message = stringResource(R.string.happy_birthday_text),
+                from = stringResource(R.string.signature_text),
+                modifier = Modifier.padding(innerPadding)
+            )
         }
     }
 }
