9794b297bac59e6fe78d6e18c73f1c4f559a26a0
Kndt <nkiet84@gmail.com>
2025-10-15 16:34:16 +0700

    Update code UI project Business Card

--- business-card/app/src/main/java/com/example/businesscard/MainActivity.kt
+++ business-card/app/src/main/java/com/example/businesscard/MainActivity.kt
@@ -5,12 +5,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Call
 import androidx.compose.material.icons.filled.Email
@@ -21,8 +23,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.businesscard.ui.theme.BusinessCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -43,41 +51,73 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Card(modifier: Modifier = Modifier) {
-    Column(modifier = modifier.fillMaxSize()) {
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center,
-            modifier = Modifier
-                .fillMaxSize()
-                .weight(3.0f)
-        ) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+            .fillMaxSize()
+            .background(Color(0xFFd2e7d4))
+    ) {
+        Body(modifier = Modifier.weight(3.0f))
+        Footer(modifier = Modifier.weight(1.0f))
+    }
+}
+
+@Composable
+fun Body(modifier: Modifier = Modifier) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier
+    ) {
+        Box(Modifier
+            .size(128.dp)
+            .fillMaxSize()) {
             Image(
                 painter = painterResource(R.drawable.android_image),
-                contentDescription = null
+                contentDescription = null,
+                contentScale = ContentScale.Crop
             )
-            Text(text = "Jennifer Doe")
-            Text(text = "Android Developer Extraordinaire")
         }
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center,
-            modifier = Modifier
-                .fillMaxSize()
-                .weight(1.0f)
-        ) {
-            Row {
-                Icon(imageVector = Icons.Default.Call, contentDescription = null)
-                Text(text = "+11 (123) 444 555 666")
-            }
-            Row {
-                Icon(imageVector = Icons.Default.Share, contentDescription = null)
-                Text(text = "@AndroidDev")
-            }
-            Row {
-                Icon(imageVector = Icons.Default.Email, contentDescription = null)
-                Text(text = "jen.doe@android.com")
-            }
-        }
+        Text(
+            text = stringResource(R.string.owner_name),
+            fontSize = 46.sp,
+            modifier = Modifier.padding(8.dp)
+        )
+        Text(
+            text = stringResource(R.string.owner_title),
+            color = Color(0xFF045b30)
+        )
+    }
+}
+
+@Composable
+fun Footer(modifier: Modifier = Modifier) {
+    Column(
+        verticalArrangement = Arrangement.SpaceEvenly,
+        modifier = modifier
+    ) {
+        IconTextRow(icon = Icons.Default.Call, text = stringResource(R.string.owner_phone))
+        IconTextRow(icon = Icons.Default.Share, text = stringResource(R.string.owner_share))
+        IconTextRow(icon = Icons.Default.Email, text = stringResource(R.string.owner_mail))
+    }
+}
+
+@Composable
+fun IconTextRow(
+    icon: ImageVector,
+    text: String,
+    modifier: Modifier = Modifier
+) {
+    Row {
+        Icon(
+            imageVector = icon,
+            contentDescription = null,
+            tint = Color(0xFF016d3b),
+            modifier = Modifier.padding(end = 16.dp)
+        )
+        Text(
+            text = text,
+        )
     }
 }
 
