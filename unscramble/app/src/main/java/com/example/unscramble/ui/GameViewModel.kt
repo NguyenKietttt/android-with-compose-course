74e0b3886bc764efae790ee55ecc6035725c3e8a
Kndt <nkiet84@gmail.com>
2025-10-30 15:16:15 +0700

    Update logic project Unscramble

--- unscramble/app/src/main/java/com/example/unscramble/ui/GameViewModel.kt
+++ unscramble/app/src/main/java/com/example/unscramble/ui/GameViewModel.kt
@@ -5,6 +5,12 @@ import com.example.unscramble.data.allWords
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import com.example.unscramble.data.MAX_NO_OF_WORDS
+import com.example.unscramble.data.SCORE_INCREASE
+import kotlinx.coroutines.flow.update
 
 class GameViewModel : ViewModel() {
     private val _uiState = MutableStateFlow(GameUiState())
@@ -13,13 +19,16 @@ class GameViewModel : ViewModel() {
     private lateinit var currentWord: String
     private var usedWords: MutableSet<String> = mutableSetOf()
 
+    var userGuess by mutableStateOf("")
+        private set
+
     private fun pickRandomWordAndShuffle(): String {
         currentWord = allWords.random()
         if (usedWords.contains(currentWord)) {
             return pickRandomWordAndShuffle()
         } else {
             usedWords.add(currentWord)
-            return pickRandomWordAndShuffle()
+            return shuffleCurrentWord(currentWord)
         }
     }
 
@@ -35,6 +44,47 @@ class GameViewModel : ViewModel() {
     fun resetGame() {
         usedWords.clear()
         _uiState.value = GameUiState(currentScrambleWord = pickRandomWordAndShuffle())
+    }
+
+    fun updateUserGuess(guessWord: String) {
+        userGuess = guessWord
+    }
+
+    fun checkUserGuess() {
+        if (userGuess.equals(currentWord, ignoreCase = true)) {
+            val updatedScore = _uiState.value.score.plus(SCORE_INCREASE)
+            updateGameState(updatedScore)
+        } else {
+            _uiState.update { currentState -> currentState.copy(isGuessedWordWrong = true) }
+        }
+
+        updateUserGuess("")
+    }
+
+    private fun updateGameState(updatedScore: Int) {
+        if (usedWords.size == MAX_NO_OF_WORDS) {
+            _uiState.update { currentState ->
+                currentState.copy(
+                    isGuessedWordWrong = false,
+                    score = updatedScore,
+                    isGameOver = true
+                )
+            }
+        } else {
+            _uiState.update { currentState ->
+                currentState.copy(
+                    isGuessedWordWrong = false,
+                    currentScrambleWord = pickRandomWordAndShuffle(),
+                    score = updatedScore,
+                    currentWordCount = currentState.currentWordCount.inc()
+                )
+            }
+        }
+    }
+
+    fun skipWord() {
+        updateGameState(_uiState.value.score)
+        updateUserGuess("")
     }
 
     init {
