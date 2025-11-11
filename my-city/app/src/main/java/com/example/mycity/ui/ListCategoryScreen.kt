62ed611649c37f10268a287509d737264c09d0cc
Kndt <nkiet84@gmail.com>
2025-11-11 07:27:36 +0700

    Add ListCategoryScreen & ListLocationScreen

---
+++ my-city/app/src/main/java/com/example/mycity/ui/ListCategoryScreen.kt
@@ -0,0 +1,43 @@
+package com.example.mycity.ui
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.mycity.data.DataSource
+
+@Composable
+fun ListCategoryScreen(
+    listCategory: List<String>,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    LazyColumn(modifier = modifier) {
+        items(listCategory) { category ->
+            Box(
+                modifier = Modifier
+                    .padding(16.dp)
+                    .clickable(onClick = onClick)
+            ) {
+                Text(text = category)
+            }
+            HorizontalDivider()
+        }
+    }
+}
+
+@Preview
+@Composable
+fun ListCategoryScreenPreview() {
+    ListCategoryScreen(
+        listCategory = DataSource.getListCategory(),
+        onClick = { }
+    )
+}
---
+++ my-city/app/src/main/java/com/example/mycity/ui/ListLocationScreen.kt
@@ -0,0 +1,45 @@
+package com.example.mycity.ui
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.mycity.data.DataSource
+import com.example.mycity.model.Location
+
+@Composable
+fun ListLocationScreen(
+    listLocation: List<Location>,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    LazyColumn(modifier = modifier) {
+        items(listLocation) { location ->
+            Box(
+                modifier = Modifier
+                    .padding(16.dp)
+                    .clickable(onClick = onClick)
+            ) {
+                Text(text = location.name)
+            }
+            HorizontalDivider()
+        }
+    }
+}
+
+@Preview
+@Composable
+fun ListLocationScreenPreview() {
+    val defaultCategory = DataSource.getDefaultCategory()
+    ListLocationScreen(
+        listLocation = DataSource.getListLocationByCategory(defaultCategory),
+        onClick = { }
+    )
+}
