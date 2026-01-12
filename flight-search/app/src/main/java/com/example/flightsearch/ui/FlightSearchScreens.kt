5064842876d733e17eac977651750ab8ff423578
Kndt <nkiet84@gmail.com>
2026-01-12 07:33:09 +0700

    Add SearchResult in project Flight Search

--- flight-search/app/src/main/java/com/example/flightsearch/ui/FlightSearchScreens.kt
+++ flight-search/app/src/main/java/com/example/flightsearch/ui/FlightSearchScreens.kt
@@ -2,16 +2,23 @@ package com.example.flightsearch.ui
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SearchBar
@@ -20,6 +27,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -27,8 +35,9 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.flightsearch.data.Airport
 
 @Composable
-fun FlightSearchApp(viewModel: FlightSearchViewModel = viewModel(factory = FlightSearchViewModel.factory)) {
-
+fun FlightSearchApp(
+    viewModel: FlightSearchViewModel = viewModel(factory = FlightSearchViewModel.factory)
+) {
     val searchKeyword by viewModel.searchKeyword.collectAsState()
     val isSearchBarExpanded by viewModel.isSearchBarExpanded.collectAsState()
     val airports by viewModel.airports.collectAsState()
@@ -38,11 +47,11 @@ fun FlightSearchApp(viewModel: FlightSearchViewModel = viewModel(factory = Fligh
             airports = airports,
             query = searchKeyword,
             onQueryChange = { viewModel.updateSearchKeyword(it) },
-            onSearch = {  },
+            onSearch = { },
             expanded = isSearchBarExpanded,
-            onExpandedChange =  { viewModel.updateSearchBarExpandStatus(it) },
+            onExpandedChange = { viewModel.updateSearchBarExpandStatus(it) },
             onCloseClick = {
-                if(searchKeyword.isEmpty())
+                if (searchKeyword.isEmpty())
                     viewModel.updateSearchBarExpandStatus(false)
                 else
                     viewModel.updateSearchKeyword("")
@@ -73,7 +82,9 @@ fun SearchScreen(
                 expanded = expanded,
                 onExpandedChange = onExpandedChange,
                 placeholder = { Text("Search") },
-                leadingIcon = { Icon(imageVector = Icons.Default.Search, contentDescription = "Search Icon") },
+                leadingIcon = {
+                    Icon(imageVector = Icons.Default.Search, contentDescription = "Search Icon")
+                },
                 trailingIcon = {
                     if (expanded) {
                         Icon(
@@ -91,9 +102,10 @@ fun SearchScreen(
     ) {
         LazyColumn {
             items(airports) { airport ->
-                SearchSuggestion(
+                AirportInfo(
                     airport = airport,
-                    onClick = { }
+                    onClick = { },
+                    modifier = Modifier.padding(8.dp)
                 )
             }
         }
@@ -101,27 +113,103 @@ fun SearchScreen(
 }
 
 @Composable
-fun SearchSuggestion(
-    airport: Airport,
-    onClick: () -> Unit,
+fun SearchResult(
+    departureAirport: Airport,
+    destinationAirport: Airport,
+    isFavourite: Boolean,
     modifier: Modifier = Modifier
 ) {
-    Column (
-        modifier = modifier
-            .clickable(onClick = onClick)
-            .padding(8.dp)
-    ) {
-        Text(
-            text = airport.iataCode,
-            style = MaterialTheme.typography.titleLarge,
-        )
-        Text(text = airport.fullName)
+    Card(modifier = modifier) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding(8.dp)
+        ) {
+            Column {
+                Text(
+                    text = "DEPART",
+                    style = MaterialTheme.typography.bodySmall)
+                AirportInfo(
+                    airport = departureAirport,
+                    onClick = { }
+                )
+                Spacer(Modifier.height(8.dp))
+                Text(
+                    text = "ARRIVE",
+                    style = MaterialTheme.typography.bodySmall
+                )
+                AirportInfo(
+                    airport = destinationAirport,
+                    onClick = { }
+                )
+            }
+            IconButton(onClick = { }) {
+                Icon(
+                    imageVector = Icons.Default.Star,
+                    contentDescription = "Favourite"
+                )
+            }
+        }
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun SearchSuggestionPreview() {
-    val previewAirport = Airport(id = 0, iataCode = "VIE", fullName = "Vienna International Airport", numOfPassengersPerYear = 200)
-    SearchSuggestion(airport = previewAirport, onClick = { })
+fun SearchResultPreview() {
+    val departureAirport = Airport(
+        id = 0,
+        iataCode = "VIE",
+        fullName = "Vienna International Airport",
+        numOfPassengersPerYear = 200
+    )
+
+    val destinationAirportAirport = Airport(
+        id = 1,
+        iataCode = "ABC",
+        fullName = "ABC International Airport",
+        numOfPassengersPerYear = 500
+    )
+
+    SearchResult(
+        departureAirport = departureAirport,
+        destinationAirport = destinationAirportAirport,
+        isFavourite = false
+    )
+}
+
+@Composable
+fun AirportInfo(
+    airport: Airport,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier.clickable(onClick = onClick)
+    ) {
+        Text(
+            text = airport.iataCode,
+            style = MaterialTheme.typography.titleSmall,
+        )
+        Spacer(Modifier.width(8.dp))
+        Text(
+            text = airport.fullName,
+            style = MaterialTheme.typography.bodySmall
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun AirportInfoPreview() {
+    val airport = Airport(
+        id = 0,
+        iataCode = "VIE",
+        fullName = "Vienna International Airport",
+        numOfPassengersPerYear = 200
+    )
+    AirportInfo(
+        airport = airport,
+        onClick = { },
+        modifier = Modifier.padding(8.dp)
+    )
 }
